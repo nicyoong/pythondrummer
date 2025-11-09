113bb5bb56db77ad9ff9f19e37afe98967088ff6
Nic <73386479+nicyoong@users.noreply.github.com>
2025-11-10 04:43:21 +0800

    load samples

--- drums.py
+++ drums.py
@@ -8,4 +8,9 @@ class DrumMachine:
         self.beat_duration = 60 / bpm
         self.samples = {}
 
+    def load_samples(self, kick_path, snare_path, hihat_path):
+        self.samples["kick"] = sa.WaveObject.from_wave_file(kick_path)
+        self.samples["snare"] = sa.WaveObject.from_wave_file(snare_path)
+        self.samples["hihat"] = sa.WaveObject.from_wave_file(hihat_path)
+
 
