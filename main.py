7772ee105249baae43abf9dce5bbf6ebd66b704a
Nic <73386479+nicyoong@users.noreply.github.com>
2025-11-10 16:48:30 +0800

    change to config json
    earlier code was hardcoded but I change to config json to avoid hardcoding

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from drums import DrumMachine
 
 if __name__ == "__main__":
     dm = DrumMachine(bpm=100)
-    dm.load_samples("kick.wav", "snare.wav", "hihat.wav")
+    dm.load_samples("config.json")
     dm.loop()
